e82adee293aeeee3838f0a96ac9c0d0b7506bfb2
emma <eqbrown@isf.edu.hk>
2025-08-19 08:18:53 +0800

    rename file, setup solution

---
+++ word_finder.py
@@ -0,0 +1,25 @@
+from list_of_words import five_letter_words
+import sys 
+
+def get_possible_words(good_letters, bad_letters, word_list): 
+    possible_words = []
+    for word in word_list: 
+ 
+        if set(good_letters).issubset(set(word)):
+            count_bad = 0
+            
+            for letter in bad_letters: 
+                if letter in word:    
+                    count_bad += 1
+
+            if count_bad == 0:
+                possible_words.append(word) 
+
+    return possible_words
+
+
+if __name__ == "__main__":
+    possible_words = get_possible_words(sys.argv[1], sys.argv[2], five_letter_words)
+    print(possible_words)
+     
+        
